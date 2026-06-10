0c74be8fd8304c7081fd11f30cb39abb8c12d3a6
APPU <appushafa1@gmail.com>
2026-06-10 12:56:38 +0530

    fix: ignore .pt model removal and serve media in production

--- mining_safety_project/urls.py
+++ mining_safety_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +13,7 @@ urlpatterns = [
     path('detection/', include('detection.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# Serve media files even in production (DEBUG=False)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
